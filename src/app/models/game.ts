d81c8b70dabc0397cf69a451f9d3ddc6e5466219
Julien Usson <usson.julien@gmail.com>
2017-03-30 17:57:52 +0200

    consoles displaying

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,7 +1,10 @@
+import { Console } from './console';
+
 export class Game {
     id: number;
     name: string;
     playability: number;
     graphics: number;
     interest: number;
+    consoles: Console[];
 }
