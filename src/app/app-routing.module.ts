55f52fe8bc9e334e4b48a6918632915ee7cb9738
JulienUsson <usson.julien@gmail.com>
2017-04-07 21:34:45 +0200

    better routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { GamesComponent } from './games/games.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/games/1', pathMatch: 'full' },
     { path: 'games/:page', component: GamesComponent },
+    { path: '**', redirectTo: '/games/' },
 ];
 
 @NgModule({
